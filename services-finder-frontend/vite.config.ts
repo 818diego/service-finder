9ee0028dc19750512d2bf6793d6ba9c27496d12c
818diego <diego.merino@uabc.edu.mx>
2024-11-14 15:59:12 -0700

    change configuration for deployed

--- services-finder-frontend/vite.config.ts
+++ services-finder-frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Permite que Vite escuche en 0.0.0.0 (IP pública).
+    port: 5173  // Especifica el puerto en el que quieres que se ejecute Vite.
+  }
 });
